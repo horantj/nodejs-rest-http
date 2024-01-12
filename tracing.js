e72255c1ab811c8e68e39b883b5ee89f70559940
TJ Horan <38511473+horantj@users.noreply.github.com>
2024-01-12 07:04:06 -0700

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -3,13 +3,13 @@ const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventi
 const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
 const { trace } = require('@opentelemetry/api');
-const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 
-const exporter = new JaegerExporter({
-  endpoint: 'http://jaeger-all-in-one-inmemory-collector.opentelemetry-js-rhosdt.svc:14268/api/traces'
+const exporter = new OTLPTraceExporter({
+    url: "http://otel-gateway-collector.openshift-opentelemetry-operator.svc.cluster.local:4318/v1/traces"
 });
 
 const provider = new NodeTracerProvider({
